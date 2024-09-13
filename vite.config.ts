43bfbc3567db00906518bf9cd63ba6828d646860
Zuaki <67538904+Zuaki21@users.noreply.github.com>
2024-09-13 16:19:38 +0900

    デプロイ時のルーティング修正

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/VtuberFukyou/' : '/', // デプロイ時はリポジトリ名をベースに
+  base: process.env.NODE_ENV === 'production' ? '/FAVE/' : '/', // デプロイ時はリポジトリ名をベースに
 });
